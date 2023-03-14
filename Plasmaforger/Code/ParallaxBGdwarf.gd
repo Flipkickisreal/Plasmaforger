e72f1e3b458cd186cab14220c7e21da929f89727
Flipkickisreal <42154453+Flipkickisreal@users.noreply.github.com>
2023-03-14 16:05:24 -0400

    Implemented gunbuilder, added dwarf background.
    added four new background layers.

---
+++ Plasmaforger/Code/ParallaxBGdwarf.gd
@@ -0,0 +1,18 @@
+extends ParallaxBackground
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_planetbuttontwo_pressed() -> void:
+	await get_tree().create_timer(5.0).timeout
+	show()
+	while get_parent().get_parent().Plasmaplaying == true:
+		await get_tree().create_timer(0.3).timeout
+		scroll_offset.x += 2
